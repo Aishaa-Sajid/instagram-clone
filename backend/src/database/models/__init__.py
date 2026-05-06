bf44b2f77845f4c6f5742821347613cd02bf9527
Ayesha <aishasajid647@gmail.com>
2026-05-06 22:56:32 +0500

    "fixes: resolve conflicts in branches"

--- backend/src/database/models/__init__.py
+++ backend/src/database/models/__init__.py
@@ -4,8 +4,5 @@ from src.database.models.post import Post
 from src.database.models.post_image import PostImage
 from src.database.models.like import Like
 from src.database.models.comment import Comment
-from src.database.models.story import Story
-from src.database.models.follow import Follow
 
-__all__ = ["Base", "User", "Post", "PostImage", "Like", "Comment", "Story", "Follow"]
-
+__all__ = ["Base", "User", "Post", "PostImage", "Like", "Comment"]
